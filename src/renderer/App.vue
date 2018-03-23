f1c4fb816821da0a939fdaf0ccf2aa1050ba8844
LHZQAQ <506394403@qq.com>
2018-03-23 14:21:43 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'endome'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
